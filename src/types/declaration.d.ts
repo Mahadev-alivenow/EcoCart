ad385288bee0fca1ce3c4d82967332fdf85907cc
ChallagidadMahadev <madhavan863@gmail.com>
2024-11-19 15:14:13 +0530

    Fix: Add module declarations for missing types

---
+++ src/types/declaration.d.ts
@@ -0,0 +1,8 @@
+declare module '@radix-ui/react-alert-dialog';
+declare module '@radix-ui/react-aspect-ratio';
+declare module '@radix-ui/react-avatar';
+// Add other missing modules here
+declare module 'embla-carousel-react';
+declare module 'vaul';
+declare module 'input-otp';
+declare module 'sonner';
